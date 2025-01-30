9a9baaf6f52b377982cff9065f4dca90228db42f
Harshit Pradhan <73273384+Harshitpradhan7@users.noreply.github.com>
2025-01-30 18:04:37 +0530

    projects-link-removed
    Adding projects link in a while after some updates

--- src/components/ExtraComps/ProjectData.js
+++ src/components/ExtraComps/ProjectData.js
@@ -2,15 +2,15 @@ const projects = [{
     title: "Task Manager App",
     description: "A simple task management app with Firebase.",
     tech: ["React", "Firebase","Tailwind","Machine Learning"],
-    github: "https://github.com/yourname/task-manager",
-    demo: "https://taskmanager.vercel.app",
+    github: "https://github.com/Harshitpradhan7",
+    demo: "https://portfolio-steel-psi-48.vercel.app/",
 },
 {
     title: "Gym Web App",
     description: "A simple app for local Gym",
     tech: ["React", "Firebase", "Tailwind", "MongoDB"],
-    github: "https://github.com/yourname/task-manager",
-    demo: "https://taskmanager.vercel.app",
+    github: "https://github.com/Harshitpradhan7",
+    demo: "https://portfolio-steel-psi-48.vercel.app/",
 },
 
 ]
